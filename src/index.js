a702c7a46e82a9cea3f3d83f9d6fe6cf51a9a3d5
George Erokhin <Georgii070684@gmail.com>
2022-01-04 12:01:39 +0300

    feat: add solution task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let number = Math.abs(n);
+    let numberStr = number.toString().split('');
+    let numberReverse = [];
+    for (let i = numberStr.length; i >= 0; i--) {
+        numberReverse.push(numberStr[i]);
+    }
+    return numberReverse.join('');
 }
